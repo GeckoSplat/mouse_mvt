312481c6884553fb3cd5e70540711b08d2c7a8fb
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-03-27 00:27:58 +0100

    messages send and recieve

--- src/looping.rs
+++ src/looping.rs
@@ -1,37 +1,87 @@
 use crate::jiggle_mouse::jiggle_mouse;
 
+
 use device_query::keymap::Keycode;
 use device_query::{DeviceQuery, DeviceState};
-use std::time::Instant;
+use tokio::sync::oneshot::Sender;
+use std::time::{Instant, Duration};
 
-pub fn looping() {
+use std::sync::mpsc::{channel,Receiver};
+use std::thread;
+
+
+
+pub async fn looping() {
+    
+
+    let (_, receiver): (_, Receiver<bool>) = channel();
+   
+
     'outerL: loop {
         let mut jiggle_counter = 1;
         let device_state = DeviceState::new();
         let keys: Vec<Keycode> = device_state.get_keys();
-
+        
         if keys.contains(&Keycode::Numpad4) {
             println!("\nJiggler running, timer started :");
             let now = Instant::now();
-            loop {
+           'inner: loop {
                 jiggle_mouse();
                 println!("jiggled {} times.", jiggle_counter);
                 jiggle_counter += 1;
-
-                let exit_key: Vec<Keycode> = device_state.get_keys();
-                if exit_key.contains(&Keycode::Numpad5) {
-                    let elapsed_time = now.elapsed();
-                    println!(
-                        "\nI jiggled for {} seconds !\nPaused jiggling.\n\nRunning in background....",
-                        elapsed_time.as_secs()
-                    );
+                
+                assert!(receiver.try_recv().is_err());
+                println!("{:?}",(receiver.try_recv().unwrap()));
+                
+                
+                let mut new_val : bool = false; 
+                if new_val == true{
                     continue 'outerL;
                 }
+                else{
+                    continue 'inner; 
+                }
+                    
+                }
             }
-        }
+        
         if keys.contains(&Keycode::Numpad6) {
             println!("\nExiting Program. Goodbye, my Leige.");
             break;
         }
+    }}
+
+    pub async fn key5(){
+        println!("started key 5");
+        'keyloop: loop{
+        thread::sleep(Duration::from_secs(2));
+        let now = Instant::now();
+        let device_state = DeviceState::new();
+        let exit_key: Vec<Keycode> = device_state.get_keys();
+        let (sender, _) = channel();
+        if exit_key.contains(&Keycode::Numpad5) {
+        let elapsed_time = now.elapsed();
+        println!(
+            "\nI jiggled for {} seconds !\nPaused jiggling.\n\nRunning in background....",
+            elapsed_time.as_secs()
+        );
+        
+        thread::spawn(move|| {
+            sender.send(true)//.expect("oh no true");
+        
+        });
+        
+        println!(" break at Key5",);
+        break;
     }
-}
+        
+        else {
+            thread::spawn(move|| {
+                sender.send(false)//.expect("oh no false");
+            
+        });
+        println!("finished Key5")
+    }
+    }}
+
+ // TODO - unwrap receiver.try_recv().unwrap() line 34and use its bool.
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use crate::looping::looping;
-
+use crate::looping::key5;
 pub mod jiggle_mouse;
 pub mod looping;
+use tokio::task;
 
 // Runs in background once launched in terminal,
 // use hotkeys to operate.
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!(
         "\nUp and running in background my Leige !\n\n
     Press Numpad 4 to jiggle.\n
     Press and Hold Numpad 5 to pause.\n
     Press Numpad 6 to Exit.\n"
     );
-
-    looping();
+    tokio::spawn(key5());
+    tokio::spawn(looping());
+    //looping().await;
+    
 }
